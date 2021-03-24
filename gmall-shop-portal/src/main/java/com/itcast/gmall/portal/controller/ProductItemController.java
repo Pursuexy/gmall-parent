3d4ab21549ed773e94fd7c709cb86980202f755e
Pursuexy <74094911+pursuexy@users.noreply.github.com>
2021-03-24 13:40:43 +0800

    项目完善02
    Signed-off-by: oxygen <1365409209@qq.com>

--- gmall-shop-portal/src/main/java/com/itcast/gmall/portal/controller/ProductItemController.java
+++ gmall-shop-portal/src/main/java/com/itcast/gmall/portal/controller/ProductItemController.java
@@ -4,15 +4,28 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.itcast.gmall.es.product.EsProduct;
 import com.itcast.gmall.pms.service.ProductService;
 import com.itcast.gmall.utils.CommonResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @RestController
 public class ProductItemController {
 
 	@Reference
 	private ProductService productService;
+
+	@Qualifier("mainThreadPoolExecutor")
+	@Autowired
+	private ThreadPoolExecutor mainThreadPoolExecutor;
+
+	@Qualifier("otherThreadPoolExecutor")
+	@Autowired
+	private ThreadPoolExecutor otherThreadPoolExecutor;
 
 	/**
 	 * 查询商品信息详情
@@ -21,6 +34,13 @@ public class ProductItemController {
 	 */
 	@GetMapping("/item/{id}.html")
 	public CommonResult productInfo(@PathVariable("id") Long id) {
+		//多线程执行任务
+		CompletableFuture.supplyAsync(() -> {
+			return "返回值";
+		}, otherThreadPoolExecutor).whenComplete((result,exception)->{
+			System.out.println("处理结果");
+			System.out.println("处理异常");
+		});
 
 		EsProduct esProduct = productService.productAllInfo(id);
 		return new CommonResult().success(esProduct);
